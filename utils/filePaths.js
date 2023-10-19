97e24faf4c2c97369debae7defe46d97bf3a75ac
ffelizp <felix.feliz-p@ab-inbev.com>
2023-10-19 11:35:37 -0400

    Task1 - Calculate Order Total Costs

--- utils/filePaths.js
+++ utils/filePaths.js
@@ -5,9 +5,13 @@ const customersFilePath = path.join(__dirname, '../data/customers.csv');
 const productsFilePath = path.join(__dirname, '../data/products.csv');
 const ordersFilePath = path.join(__dirname, '../data/orders.csv');
 
+// Output Files
+const orderPricesFilePath = path.join(__dirname, '../data/order_prices.csv');
+
 // Export Files
 module.exports = {
     customersFilePath,
     productsFilePath,
     ordersFilePath,
+    orderPricesFilePath,
 };
